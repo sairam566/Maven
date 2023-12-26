126855ea86185515666b9541853a445e7ec3e33e
sairam566 <sairam1431995@gmail.com>
2023-12-26 15:34:50 +0530

    Adding local Jar into POM file

---
+++ 02_MavenUseLocalJar/src/main/java/com/tsr/CallGreet.java
@@ -0,0 +1,10 @@
+package com.tsr;
+
+import com.tsr.greet.Greeting;
+
+public class CallGreet {
+	public static void main(String[] args) {
+		Greeting greeting = new Greeting();
+		greeting.greet();
+	}
+}
